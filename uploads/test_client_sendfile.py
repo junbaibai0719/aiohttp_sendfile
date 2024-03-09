eea3e15e26465a0b223b77e70347df3711cdaadb
junbaibai <1355767057@qq.com>
2024-03-09 19:53:21 +0800

    refactor

--- uploads/test_client_sendfile.py
+++ uploads/test_client_sendfile.py
@@ -1,11 +1,12 @@
 
 import asyncio
 import pathlib
+import time
 import aiohttp
 
 import aiofiles
 
-from aiohttp_sendfile import SendFile
+from aiohttp.payload import SendFile
 
 
 async def file_sender(file_path):
@@ -17,21 +18,27 @@ async def upload(sess:aiohttp.ClientSession, file_path:pathlib.Path):
     data = aiohttp.FormData(quote_fields=False)
     data.add_field('file', file_sender(file_path), filename=file_path.name)
     async with sess.post('http://localhost:8080/upload', data=data) as resp:
-        print(resp.request_info)
-        print(await resp.text())
+        # print(resp.request_info)
+        # print(await resp.text())
+        ...
 
 async def upload_sendfile(sess:aiohttp.ClientSession, file_path:pathlib.Path):
     data = aiohttp.FormData(quote_fields=False)
     data.add_field('file', SendFile(file_path), filename="sendfile"+file_path.name)
     async with sess.post('http://localhost:8080/upload', data=data) as resp:
-        print(resp.request_info)
-        print(await resp.text())
+        # print(resp.request_info)
+        # print(await resp.text())
+        ...
 
 async def main():
     async with aiohttp.ClientSession() as sess:
-        await upload(sess, pathlib.Path('test.py'))
-        await upload_sendfile(sess, pathlib.Path('test.py'))
-        with open("./uploads/sendfiletest.py", "rb") as fp, open("./uploads/test.py", "rb") as fp2:
+        start = time.time()
+        await asyncio.gather(*[upload(sess, pathlib.Path(__file__)) for i in range(5000)])
+        print("cost ", time.time() - start)
+        start = time.time()
+        await asyncio.gather(*[upload_sendfile(sess, pathlib.Path(__file__)) for i in range(5000)])
+        print("cost ", time.time() - start)
+        with open("./uploads/sendfile"+pathlib.Path(__file__).name, "rb") as fp, open("./uploads/"+pathlib.Path(__file__).name, "rb") as fp2:
             assert fp.read() == fp2.read()
 
 asyncio.run(main())
